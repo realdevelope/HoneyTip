902ff1ea3a5abd8276583b05ea5b4be825996822
dynam0507 <skaeodud0507@naver.com>
2019-05-29 14:44:23 +0900

    필요없는 코드 & 파일 삭제

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const WRITER_TAG_ID = 'writer';
+
+/**
+	@param text 채울 텍스트 인자
+	@param tag 텍스트 박스 태그 이름 
+*/
+function fillTextbox(text) {
+	let tagElement = document.getElementById(WRITER_TAG_ID);
+	if(tagElement == null) return;
+
+	tagElement.innerHTML = text;
+}
+
 
 /**
 	사이트의 경로를 바꿔주는 함수
@@ -5,9 +18,5 @@
 	@param src 바꿀 사이트 경로
 */
 function changePage(src) {
-	window.location.href = src;
-}
-
-function changePage(src){
 	window.location.href = src;
 }
